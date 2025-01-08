655afdb70413a3cb1792bed06f9908b49e476af8
Aryan Singh <161226371+aryan-singh18@users.noreply.github.com>
2025-01-08 18:16:35 +0530

    Add files via upload

---
+++ src/config/firebase.js
@@ -0,0 +1,99 @@
+
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, sendPasswordResetEmail, signInWithEmailAndPassword, signOut} from 'firebase/auth'
+import { collection, doc, getDocs, getFirestore, query, setDoc, where } from 'firebase/firestore'
+import { toast } from "react-toastify";
+
+// --------- Paste Your Firebase Config File Here ---------
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXdSMun7TDSBkj6I21fOIUGaqFlTSuycU",
+  authDomain: "chat-app-gs-aa2cf.firebaseapp.com",
+  projectId: "chat-app-gs-aa2cf",
+  storageBucket: "chat-app-gs-aa2cf.firebasestorage.app",
+  messagingSenderId: "800409371634",
+  appId: "1:800409371634:web:6ea1f3da8005d9dd91e16b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+const signup = async (username, email, password) => {
+    try {
+        const usersRef = collection(db,'users')
+        const q = query(usersRef,where("username", "==" ,username.toLowerCase()))
+        const querySnapshot = await getDocs(q)
+        if(querySnapshot.docs.length>0){
+            toast.error("Username already taken")
+            return 0;
+        }
+        const res = await createUserWithEmailAndPassword(auth, email, password)
+        const user = res.user
+        await setDoc(doc(db,"users",user.uid), {
+            id: user.uid,
+            username:username.toLowerCase(),
+            email,
+            name:"",
+            avatar:"",
+            bio:"Hey, There i am using chat app",
+            lastSeen: Date.now()
+        });
+        await setDoc(doc(db,"chats",user.uid), {
+            chatsData:[]
+        });
+
+    } catch (error) {
+        console.error(error)
+        toast.error(error.code.split('/')[1].split('-').join(" "))
+    }
+}
+
+const login = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password)
+    } catch (error) {
+        if (error.code) {
+            toast.error(error.code.split('/')[1].split('-').join(" "));
+        } else {
+            toast.error("An unexpected error occurred. Please try again.");
+        }
+        
+    }
+}
+
+const logout = () => {
+    signOut(auth)
+}
+
+const resetPass = async (email) => {
+    if (!email) {
+        toast.error("Enter your email")
+        return null
+    }
+    try {
+        const userRef = collection(db, "users")
+        const q = query(userRef, where("email", "==", email))
+        const querySnap = await getDocs(q)
+        if (!querySnap.empty) {
+            await sendPasswordResetEmail(auth,email)
+            toast.success("Reset Email Sent")
+        }
+        else {
+            toast.error("Email doesn't exists")
+        }
+    } catch (error) {
+        console.error(error)
+        toast.error(error.message)
+    }
+   
+}
+
+export { auth, db, login, signup, logout, resetPass};
